77b37603b4c93f85355c2cb92eb45ce81e9b6c96
Jackie <jtran2009a@gmail.com>
2020-03-10 22:16:23 -0400

    Added navigation for authentication screens

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './scenes/authentication/LoginScreen';
+import RegisterScreen from './scenes/authentication/RegisterScreen';
+import ForgotPasswordScreen from './scenes/authentication/ForgotPasswordScreen';
+
 import LeaguesPage from './scenes/LeaguesScreen'
 
+// Navigation
+const Stack = createStackNavigator();
+
 export default function App() {
+
   return (
-    <LeaguesPage />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="Forgot" component={ForgotPasswordScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ frontend/scenes/authentication/LoginScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+
+  return (
+    <View style={styles.container}>
+        <Button title="Login"/>
+        <Button title="Register" onPress={() => navigation.navigate('Register')}/>
+        <Button title="Forgot Password" onPress={() => navigation.navigate('Forgot')}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default LoginScreen;
